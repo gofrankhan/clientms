bf4ed62252df0f381afa1285918766cb8a5453eb
Md. Gofran Khan <gofran0123@gmail.com>
2023-01-08 19:45:49 +0600

    server side redirect issue solve using ob_start()

--- delete-file.php
+++ delete-file.php
@@ -1,6 +1,9 @@
 <?php
+ob_start();
 session_start();
-error_reporting(0);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 include('includes/dbconnection.php');
 if (strlen($_SESSION['clientmsaid']==0)) {
     header('location:logout.php');
